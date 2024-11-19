656dfeeb869e5c814925e6f6db5e7bec7049cf1a
AbdelmouaizKh <abdelmouaizkheddouma@gmail.com>
2024-11-19 18:15:05 +0100

    Create __init__.py

---
+++ simulation/__init__.py
@@ -0,0 +1,9 @@
+# simulation/__init__.py
+
+"""
+Evolution Simulator Package
+===========================
+This package contains modules for simulating the evolution of populations of creatures.
+"""
+
+__version__ = "0.1.0"
