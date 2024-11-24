845999b9e76b3b5580288b2b5dfccce4ec123da8
AbdelmouaizKh <abdelmouaizkheddouma@gmail.com>
2024-11-24 15:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from simulation.content import world, tree, animal
 
 def main():
     # Initialize the world
-    my_world = world(size=100, max_animals=50)
+    my_world = world(size=100, max_animals=75)
 
     # Create some trees with random DNA
-    tree_dnas = ["abcf", "abcf", "abcf"]
-    animal_dnas = ["bbbb","bbbb","bbbb","bbbb"]
+    tree_dnas = ["abcd","baef","adda"]
+    animal_dnas = ["cccc","cccc","cccc","cccc","cccc","cccc"]
     print(my_world.create_trees(tree_dnas))
     my_world.create_animals(animal_dnas)
 
     # Run a simulation step
-    print("Initial state:")
-    print(f"Trees: {len(my_world.trees)}, Animals: {len(my_world.animals)}")
-    my_world.step()
-    print("Simulation step completed!")
-    print(f"Trees: {len(my_world.trees)}, Animals: {len(my_world.animals)}")
-    my_world.step()
-    print("Simulation step completed!")
-    print(f"Trees: {len(my_world.trees)}, Animals: {len(my_world.animals)}")
+    print("initial state:"+f"animals alive: {len(my_world.animals)}")
+    for i in range(2000):
+        my_world.step()
+        print(f"animals alive: {len(my_world.animals)}")
+    print("final state:"+f"animals alive: {len(my_world.animals)}")
+    for i in range(len(my_world.animals)):
+        print(my_world.animals[i].dna)
+    
 
 if __name__ == "__main__":
     main()
