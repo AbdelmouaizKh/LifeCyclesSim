6339fa33148bff1fa8604d4fb54443c91de330a2
AbdelmouaizKh <abdelmouaizkheddouma@gmail.com>
2024-12-19 10:37:52 +0100

    Update content.py

--- simulation/content.py
+++ simulation/content.py
@@ -18,7 +18,9 @@ class creature(ABC):
 
 class animal(creature):
     animals = []
-    def __init__(self,dna,food=0):
+    def __init__(self,dna,sex,age=0,food=0):
+        self.age = age
+        self.sex = sex
         self.food = food
         self.size = None
         self.reproduction_threshold = None
@@ -30,10 +32,10 @@ class animal(creature):
 
     def express_dna(self):
         dna_mapping = {
-            'size': {"a":100,"b":80,"c":60,"d":40,"e":20,"f":10},
-            'reproduction_threshold': {"a": 100, "b": 200, "c": 300, "d": 500, "e": 750, "f": 1000},
+            'size': {"a":50,"b":40,"c":30,"d":20,"e":10,"f":5},
+            'reproduction_threshold': {"a": 50, "b": 750, "c": 100, "d": 125, "e": 150, "f": 200},
             'speed': {"a":100,"b":80,"c":60,"d":40,"e":20,"f":10},
-            'toxicity_resistance': {"a": 1, "b": 0.5, "c": 0.25, "d": 0.1, "e": 0.01, "f": 0}
+            'toxicity_resistance': {"a": 1, "b": 0.75, "c": 0.5, "d": 0.25, "e": 0.1, "f": 0}
         }
         self.size = dna_mapping['size'][self.dna[0]]
         self.reproduction_threshold = dna_mapping['reproduction_threshold'][self.dna[1]]
@@ -42,19 +44,15 @@ class animal(creature):
 
 
 
-    def reproduce(self,mutation_rate=0.2):
+    def reproduce(self,other,mutation_rate=0.1):
         
         possible_genes = "abcdef"
         
         offspring_dna = ""
-        for gene in self.dna:
-            if random.random() < mutation_rate:
-                new_gene = random.choice([g for g in possible_genes if g != gene])
-                offspring_dna += new_gene
-            else:
-                offspring_dna += gene
-        
+        for i in range(4):
+            offspring_dna += random.choices([self.dna[i],other.dna[i],random.choice(possible_genes)],[0.5*(1-mutation_rate),0.5*(1-mutation_rate),mutation_rate])[0]
         return offspring_dna
+
     def die(self,msg):
         #print(msg)
         self.alive = False
@@ -62,19 +60,22 @@ class animal(creature):
     def consume(self,fruit):
         self.food += fruit[0]
         poison_chance = fruit[1]*(1-self.toxicity_resistance)
-        if random.choices((0,1),weights=[1-poison_chance,poison_chance]):
+        if random.choices((0,1),weights=[1-poison_chance,poison_chance])[0]:
             self.die("dies of poison")
     
     def energy_consumption(self):
-        self.food -= self.size+self.speed
+        self.food -= (self.size+self.speed)//7
         if self.food <=0:
             self.die("dies of hunger")
+        elif self.age > 5:
+            self.die("dies of old age")
         
-        number_of_ofspring = self.food//self.reproduction_threshold
-        chance = random.choices((0,1),weights=[(1-self.food)/self.reproduction_threshold,self.food/self.reproduction_threshold])[0]
-        number_of_ofspring += chance
-        self.food = self.food*(1-chance)
-        return [self.reproduce() for i in range(number_of_ofspring)]
+    def mate(self,other):
+        number_of_ofspring = min(min(self.food//self.reproduction_threshold,other.food//other.reproduction_threshold),3)
+        self.food -= number_of_ofspring*self.reproduction_threshold
+        other.food -+ number_of_ofspring*other.reproduction_threshold
+        self.age += 1
+        return [self.reproduce(other) for i in range(number_of_ofspring)]
 
 
 class tree(creature):
@@ -90,9 +91,9 @@ class tree(creature):
 
     def express_dna(self):
         dna_mapping = {
-            'caloric_value': {"a":200,"b":100,"c":50,"d":10,"e":0,"f":-50},
-            'fruit_count': {"a": 5, "b": 4, "c": 3, "d": 2, "e": 1, "f": 0},
-            'size': {"a": 5, "b": 4, "c": 3, "d": 2, "e": 1, "f": 0},
+            'caloric_value': {"a":2000,"b":1500,"c":1000,"d":750,"e":200,"f":100},
+            'fruit_count': {"a": 10, "b": 7, "c": 5, "d": 3, "e": 2, "f": 1},
+            'size': {"a": 10, "b": 7, "c": 5, "d": 3, "e": 2, "f": 1},
             'toxicity': {"a": 0, "b": 0.01, "c": 0.1, "d": 0.25, "e": 0.5, "f": 1}
         }
         self.caloric_value = dna_mapping['caloric_value'][self.dna[0]]
@@ -104,7 +105,7 @@ class tree(creature):
         return [(self.caloric_value,self.toxicity) for i in range(self.fruit_count)]
     
     def host_animals(self,animals):
-        animals.sort(key=lambda x:x.size+x.speed)
+        animals.sort(key=lambda x:2*x.size+x.speed)
         fruits = self.grow_fruit()
         if len(animals)==0 or len(fruits)==0:
             return None
@@ -133,7 +134,7 @@ class world():
     def create_animals(self,animal_dnas):
         i = 0
         while self.max_animals>len(self.animals) and i<len(animal_dnas):
-            self.animals.append(animal(animal_dnas[i]))
+            self.animals.append(animal(animal_dnas[i],random.choice(["male","female"])))
             i += 1
         return animal_dnas
     
@@ -144,8 +145,9 @@ class world():
         return [self.animals[i].dna for i in range(len(self.animals))]
     
     def step(self):
-        self.animals = list(filter(lambda x:x.alive,self.animals))
         random.shuffle(self.animals)
+        male_animals = list(filter(lambda x:x.sex == "male",self.animals))
+        female_animals = list(filter(lambda x:x.sex == "female",self.animals))
         hosted_animals_counter = 0
         for i in range(len(self.trees)):
             if hosted_animals_counter == len(self.animals):
@@ -155,9 +157,11 @@ class world():
             hosted_animals_counter += self.trees[i].size
         new_animals_dnas = []
         for i in self.animals:
-            new_animals_dnas += i.energy_consumption()
-        
+            i.energy_consumption()
+        self.animals = list(filter(lambda x:x.alive,self.animals))
+        for i in range(min([len(male_animals),len(female_animals)])):
+            new_animals_dnas += male_animals[i].mate(female_animals[i])
         self.create_animals(new_animals_dnas)
-    
+
     def reset_world(self):
         self.__init__(size=self.size,max_animals=self.max_animals)
