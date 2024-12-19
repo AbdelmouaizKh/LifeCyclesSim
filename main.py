e5f9300e538cb9fe37fe6c495b036ed97d35585d
AbdelmouaizKh <abdelmouaizkheddouma@gmail.com>
2024-12-19 10:37:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from simulation.display import plot_registered_data
+from simulation.display import plot_data
 from simulation.helper_functions import world_generator, random_dna_generator
 
 def main():
-    dna_gen_pos = random_dna_generator(10,1)
-    dna_gen_mid = random_dna_generator(10,0)
-    dna_gne_nig = random_dna_generator(10,-1)
-    worlds = world_generator(5,dna_gen_pos.generate(),dna_gen_mid.generate())
+    dna_gen_pos = random_dna_generator(20,1)
+    dna_gen_mid = random_dna_generator(100,0)
+    dna_gen_nig = random_dna_generator(10,-1)
+    worlds = world_generator(5,dna_gen_nig.generate(),dna_gen_pos.generate())
     worlds.initiate()
-    plot_registered_data(worlds.simultaneous_steps(20))
+    plot_data(worlds.simultaneous_steps(40))
 
 
 if __name__ == "__main__":
